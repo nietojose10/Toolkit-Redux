afc968f8430c7cfa9fde752b764476d9b2487054
Jose <nietojose1995@gmail.com>
2023-08-05 10:13:34 +0100

    Uploading Toolkit Redux Examples

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { store } from './store'
+import { Provider } from 'react-redux'
+import { PokemonApp } from './PokemonApp.jsx'
+import { TodoApp } from './TodoApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+    <Provider store={ store }>
+      {/* <App /> */}
+      {/* <PokemonApp/> */}
+      <TodoApp/>
+
+    </Provider>
+    
+  </React.StrictMode>,
+)
